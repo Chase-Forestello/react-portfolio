6c2f1ddc4ba6891e0a74fbb94aec6c0008a5d379
Chase Forestello <chase.forestello@gmail.com>
2023-03-16 17:15:41 -0500

    Skills added to home page. Responsiveness needs work

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,6 +24,7 @@ const Home = () => {
     "e",
     "r",
   ];
+  const skillsArray = ["S", "k", "i", "l", "l", "s"]
 
   let path = window.location.pathname;
   console.log(path);
@@ -69,6 +70,43 @@ const Home = () => {
               MY PROJECTS
             </Link>
           </div>
+        </div>
+        <div className="skills-zone">
+          <text className="skills-title"><AnimatedLetters
+              letterClass={letterClass}
+              strArray={skillsArray}
+              index={22}
+            /></text>
+          <br></br>
+          <hr className="skills-hr"></hr>
+          <span className="skill">HTML</span>
+          <span className="skill">CSS</span>
+          <span className="skill">SCSS</span>
+          <span className="skill">Bootstrap</span>
+          <span className="skill">JavaScript</span>
+          <span className="skill">React</span>
+          <br></br>
+          <span className="skill">Java</span>
+          <span className="skill">Spring Boot</span>
+          <span className="skill">Hibernate</span>
+          <span className="skill">MySQL</span>
+          <span className="skill">Database Design</span>
+          <span className="skill">OOP</span>
+          <br></br>
+          <span className="skill">SDLC</span>
+          <span className="skill">REST API's</span>
+          <span className="skill">SPA's</span>
+          <span className="skill">TDD</span>
+          <span className="skill">Swagger</span>
+          <span className="skill">Verison Control with Git</span>
+          <br></br>
+          <span className="skill">GitHub</span>
+          <span className="skill">GitHub Projects</span>
+          <span className="skill">AWS</span>
+          <span className="skill">SCRUM</span>
+          <span className="skill">Paired Programming</span>
+          <span className="skill">Mobile First Development</span>
+          <hr className="skills-hr"></hr>
         </div>
       </div>
       <Loader type="pacman"></Loader>
